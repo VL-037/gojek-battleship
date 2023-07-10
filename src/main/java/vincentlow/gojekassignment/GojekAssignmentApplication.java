cd253e2dab3106b4620d16fb6dd26f6fba34923a
Vincent Low <vincent.low037@gmail.com>
2023-07-10 13:52:25 +0700

    initial commit

---
+++ src/main/java/vincentlow/gojekassignment/GojekAssignmentApplication.java
@@ -0,0 +1,13 @@
+package vincentlow.gojekassignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GojekAssignmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GojekAssignmentApplication.class, args);
+	}
+
+}
